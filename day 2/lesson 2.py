5b36ff3f90cf2b7fb94a5186de97bbfa10c84835
andriatsurtsumia <andriaanderson08@gmail.com>
2024-02-04 16:22:40 +0400

    first commit

---
+++ day 2/lesson 2.py
@@ -0,0 +1,42 @@
+name = "andria"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# = "andria" არის ცვლადისთვის მნიშვნელობა 
+
+surname = "tsurtsumia" 
+
+#print (name)
+#პრინტ ფუნქციის გადაცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "andria" #ეს არის str (string) tipis cvladi
+age = 15 #ეს არის int ( integer ) მთელი რიცხვი
+height = 180. #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი 
+
+knowns_prigraming = True #True ან Folse
+is_ugly = False #snarecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული cameLcase 
+
+
+print(name + "  " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლო
+
+
+# print(type(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+# print(name + " " + str(age))
+
+
+#homework
+
+
+# print (type(andria))
+# print (type(tsurtsumia))
+# print (15())
+# print (180.4())
