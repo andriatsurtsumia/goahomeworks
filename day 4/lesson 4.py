130940f3a408543176ef682b2c61c27e2446819c
andriatsurtsumia <andriaanderson08@gmail.com>
2024-02-18 17:01:00 +0400

    fourfth commit

--- day 4/lesson 4.py
+++ day 4/lesson 4.py
@@ -3,63 +3,63 @@
 
 name = "ekaterine"
 surname = "gogebashvili"
-age = "48" 
+age = 48 
 print(name + "  " + surname)
-height = "165"
+height = 165
 
-name = "petre"
+name21 = "petre"
 surname = "tsurtsumia"
-age = "48"
+age = 48
 print(name + "  " + surname) 
-heught = "190"
+heught = 190
 
 name = "nuca"
 surname = "tsurtsumia"
-age = "20"
+age = 20
 print(name + "  " + surname) 
-height = "166"
+height = 166
 
 name = "ekaterine"
-age = "48"
+age = 48
 print(age + 2)
 
 name = "petre"
-age = "48"
+age = 48
 print(age + 2) 
 
 name = "nuca"
-age = "20"
+age = 20
 print(age + 2)
 
 name = "andria"
 surname = "tsurtsumia"
-age = "15"
+age = 15
 print(name + "  " + surname) 
 height = 180.6
 
 name = "andria"
-age = "15"
+age = 15
 print (age + 2)
 
 
 #დავლების მეორე ვერსია 
 
-# print (type(ekaterine))
+# print (type(name1))
 # print (type(gogebashvili))
 # print (type(48))
 # print (type(165))
 
-# print (type(petre))
+# print (type(name2))
 # print (type(tsurtsumia))
 # print (type(48))
 # print (type(190))
 
-# print (type(nuca))
+# print (type(name3))
 # print (type(tsurtsumia))
 # print (type(20))
 # print (type(166))
 
-# print (type(andria))
+# print (type(name4))
 # print (type(tsurtsumia))
 # print (15())
 # print (180.6())
