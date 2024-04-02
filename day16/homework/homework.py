312dba0056efc928a1232c86b8d0c980958e68b1
andriatsurtsumia <andriaanderson08@gmail.com>
2024-04-02 22:09:58 +0400

    tvelth commit

---
+++ day16/homework/homework.py
@@ -0,0 +1,22 @@
+# def sum(listen):
+#     even_list = []
+#     odd_list = [] 
+#     for i in listen: 
+#         if i % 2 == 0: 
+#             even_list.append(i) 
+#         else:
+#             odd_list.append(i) 
+#     sum_even = sum(even_list) 
+#     sum_odd = sum(odd_list) 
+#     return [sum_even, sum_odd] 
+
+
+# print(sum([1,2,3,4,5]))
+# print(sum([1,2,3,4,5,6,7,8,9,10])) 
+
+# even_list = [2,4]
+# sum(even_list) 
+
+# def greet(name):
+#     print("Hello {name}") 
+# greet("andria")
---
+++ day17/classwork/classwork.py
@@ -0,0 +1,11 @@
+#idk = ["Goal", "Oriented", "Academy", "is", "Best"]
+#new_idk = " ".join(idk)
+
+#print(new_idk)    
+
+
+def sentence(words): 
+    new_sentenece = " ". join(words) 
+
+
+print(sentence(["Goal", "Oriented", "Academy", " is", "best"]))
