2261a886aeddbe099d79d5119cb2f6b110bdd9e6
andriatsurtsumia <andriaanderson08@gmail.com>
2024-04-07 18:05:23 +0400

    treeth commit

---
+++ day18/homework/homework.py
@@ -0,0 +1,48 @@
+# მომხმარებელს შემოატანინეთ სახელი. თქვენი დავალებაა, რომ შეადაროთ სახელი მის lowercase ვარიანტთან == ოპერატორის გამოყენებით
+
+#name = input("please enter your name")
+
+#print(name.lower())
+
+# მოხმარებელს შემოატანინეთ სიტყვა და საძიებელი ასო. თქვენი დავალებაა, რომ სიტყვაში პირველივე შემხვედრი ამ ასოს ინდექსი დაბეჭდოთ, არ არსებობის შემთხვევაში კი უბრალოდ -1.
+
+# name = input("please enter your word") 
+# for index in range(len(name)): 
+#     if name[index] == name: 
+#         print(index) 
+
+# print(name)  
+
+
+
+#family = ["vano" , "vano1" , "vano2"]
+
+#new_str = "-".join(family)
+#print(new_str) 
+
+
+# list = []
+
+# for i in range(5):
+#     element = input("enter name: ")
+#     list.append(element)
+
+# connect = input("chose: '-' , '+' '/': ")
+
+# n_str = connect.join(list[:5])
+
+# print(n_str)
+
+
+
+
+# სიაში შეინახეთ თქვენი სახელი და გვარი. capitalize მეთოდის გამოყენებით მასივის ყველა ელემენტზე მოახდინეთ მუშაობა და ბოლოს დაბეჭდეთ უკვე შეცვლილი სია.
+
+
+
+
+# obj = ["andria", "tsurtsumia"]
+
+# x = obj.capitalize()
+
+# print (x)
