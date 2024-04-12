1fc450b29418774818de8b6e10c4bcdfaa6f87de
andriatsurtsumia <andriaanderson08@gmail.com>
2024-04-13 00:39:39 +0400

    fourtheenth commit

---
+++ day 2/homework/day2.py
@@ -0,0 +1,7 @@
+#homework
+
+
+# print (type(andria))
+# print (type(tsurtsumia))
+# print (15())
+# print (180.4())
---
+++ day15/homework/homework.py
@@ -0,0 +1,8 @@
+def print_eight_times(name,hello):
+    for i in range(hello):
+        print(name)
+        
+
+print_eight_times("hello Andria",5)
+
+print_eight_times("hello Vano",2) 
