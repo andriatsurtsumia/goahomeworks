c8660d8c4a9ecfacccf5a46e9fce8923af42ece9
andriatsurtsumia <andriaanderson08@gmail.com>
2024-04-22 23:45:06 +0400

    sixtheen commit

---
+++ day22/classwork/classwor.py
@@ -0,0 +1,21 @@
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for i in range():
+# element = input("enter name: ")
+# list.pop(element)
+# my_list.pop()
+# my_list.pop() 
+
+# print(my_list)
+
+
+
+
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
