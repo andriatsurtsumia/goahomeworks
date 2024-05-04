bb9b1027a7db5080c9a5d5fe1b08f328c4c4298d
andriatsurtsumia <andriaanderson08@gmail.com>
2024-05-04 10:30:13 +0400

    twentheen commit

---
+++ day26/classwork/classwork.py
@@ -0,0 +1,37 @@
+# name = input("please enter your name")
+
+# print(name.lower()) 
+# print(name.upper()) 
+# print(name.capitalize()) 
+
+
+
+# def find_idex(word, find_char) :
+#     for i in range(len(word)):
+#         if word[index] == find_char:
+#             return index
+
+# def manual_pop(collection, remove_index):
+#     if remove_index >= len(collection):
+#         return "Index out of range"
+    
+#     result = []
+
+#     for index in range(len(collection)):
+#         if index != remove_index:
+#             result.append(collection[index])
+
+#     return result
+
+# print(manual_pop(["andria", "vano"], 1))
+
+
+
+
+# def manual_len(collection):
+#     count = 0
+#     for item in collection:
+#         count = count + 1
+#     return count
+
+# print(manual_len(["vano", "andria", 3, True, "Nika"])) 
---
+++ day26/homework/homework.py
@@ -0,0 +1,5 @@
+name = input("please enter your name")
+
+print(name.lower()) #lower სიმბოლოებს გარდაქმნის პატარა ასოებად და შემდეგ ტერმინალზეც გამოდის პატარა ასოებით
+print(name.upper()) #upper სიმბოლოებს გარდაქმნის დიდ ასოებად 
+print(name.capitalize())
