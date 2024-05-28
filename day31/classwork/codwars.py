39d5b5cd8c3f12d90053fad9e344ed98b16053b9
andriatsurtsumia <andriaanderson08@gmail.com>
2024-05-28 09:11:31 +0400

    twentheethree commit

---
+++ day31/classwork/codwars.py
@@ -0,0 +1,36 @@
+# def fake_bin(x):
+#     result = ''
+    
+#     for char in x:
+#         if int(char) < 5:
+#             result = result + "0"
+#         else:
+#             result = result + "1"
+
+
+
+
+#             def get_count(sentence):
+#     to_count = ["a", "e", "i", "o", "u"]
+#     res = 0
+#     for i in to_count:
+#         for j in sentence:
+#             if i.lower() == j.lower():
+#                 res += 1
+#     return res
+
+
+# def square_digits(num):
+#     to_return = ""
+#     for element in str(num):
+#         to_return = to_return + element f'{int(element) * int(element)}  
+#         return int(to_return)
+
+
+#         def filter_list(l):
+#     list1 = []
+#     for element in l:
+#         if type(element) == int:
+#             list1.append(element)
+            
+#             return list1
