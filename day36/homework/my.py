da6f02218391d2751098923d3657405d25eaed42
andriatsurtsumia <andriaanderson08@gmail.com>
2024-06-05 11:06:29 +0400

    twenthsix commit

---
+++ day36/homework/my.py
@@ -0,0 +1,41 @@
+# About_me = {
+
+#     "name" : "andria"
+#     "surname" : "tsurstumia"
+#     "age" : "16"
+
+# } 
+
+# to_join = About_me.values
+# values_joined = " "join([str i for i in to_join]) 
+
+# print(values_joined)
+
+
+# About_me = {
+
+#      "name" : "Giorgi"
+#      "surname" : "matureli"
+#      "age" : "14"
+
+#  } 
+
+#  to_join = About_me.values
+#  values_joined = " "join([str i for i in to_join]) 
+
+#  print(values_joined)
+
+
+
+# About_me = {
+
+#      "name" : "Vano"
+#      "surname" : "Motiashvili"
+#      "age" : "17"
+
+#  } 
+
+#  to_join = About_me.values
+#  values_joined = " "join([str i for i in to_join]) 
+
+#  print(values_joined)
